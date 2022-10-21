7e9b26d212a66b4a8f30d5205befc08027386398
Vicent Botella <vb@interap.de>
2022-10-21 09:58:47 +0200

    first method created

--- FakeCompany-Application.Cons/Program.cs
+++ FakeCompany-Application.Cons/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using FakeCompany_Application.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FakeCompany_Application.Cons
+{
+    public class Program
+    {
+        private static FakeCompanyContext context = new FakeCompanyContext();
+
+        private static void Main(string[] args)
+        {
+            GetEmployees();
+            Console.WriteLine("Press any key...");
+            Console.ReadKey();
+        }
+
+        private static void GetEmployees()
+        {
+            List<Employee> employees = context.Employees.ToList();
+
+            Console.WriteLine("");
+            foreach (var employee in employees)
+            {
+                Console.WriteLine(employee.EmployeeName);
+                Console.WriteLine("");
+            }
+        }
+    }
+}
--- FakeCompany-Application.Data/Models/FakeCompanyContext.cs
+++ FakeCompany-Application.Data/Models/FakeCompanyContext.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.Extensions.Logging;
 
 namespace FakeCompany_Application.Data.Models
 {
@@ -27,8 +28,9 @@ namespace FakeCompany_Application.Data.Models
         {
             if (!optionsBuilder.IsConfigured)
             {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=FakeCompany;Trusted_Connection=True;");
+                optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=FakeCompany;Trusted_Connection=True;")
+                    .LogTo(Console.Write, new[] { DbLoggerCategory.Database.Command.Name, DbLoggerCategory.Database.Transaction.Name }, LogLevel.Debug)
+                    .EnableSensitiveDataLogging(); ;
             }
         }
 
